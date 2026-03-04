a02dc61f97ec13b1d35c853620e823fc129bfa25
madeosdev <matheosdev@proton.me>
2026-03-04 19:28:50 -0300

    Add files via upload

---
+++ 2026_03_04_ex_03/app.js
@@ -0,0 +1,5 @@
+let num_1 = parseFloat(prompt('Digite o primeiro número:'))
+let num_2 = parseFloat(prompt('Digite o segundo número:'))
+let soma = num_1 + num_2
+
+document.getElementById('resultado').innerHTML = soma
