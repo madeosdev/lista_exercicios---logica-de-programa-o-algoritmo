9fb774459824e7f3121c76f30502f8cc98d78a16
madeosdev <matheosdev@proton.me>
2026-03-04 19:50:53 -0300

    Add files via upload

---
+++ 2026_03_04_ex_08/app.js
@@ -0,0 +1,6 @@
+let ano_novo_1 = 'Feliz ano novo!'
+let ano_novo_2 = 'Feliz ano novo!'
+let ano_novo_3 = 'Feliz ano novo!'
+document.getElementById('resultado').innerHTML = ano_novo_1
+document.getElementById('resultado2').innerHTML = ano_novo_2
+document.getElementById('resultado3').innerHTML = ano_novo_3
