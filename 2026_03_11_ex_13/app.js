f52fc9a9ff68d80d0fb15dc636a6e74d8eae629a
madeosdev <matheosdev@proton.me>
2026-03-11 19:09:11 -0300

    Add files via upload

---
+++ 2026_03_11_ex_13/app.js
@@ -0,0 +1,6 @@
+let frase1 = 'Aprendendo algoritmo'
+let frase2 = 'e Fazendo muito exercício'
+let frase3 = 'Primeiro fazendo exercício em "Javascript". '
+document.getElementById('resultado1').innerHTML = frase1
+document.getElementById('resultado2').innerHTML = frase2
+document.getElementById ('resultado3').innerHTML = frase3
