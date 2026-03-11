9565eadee95b09719ec897b21de222789f4a4f7d
madeosdev <matheosdev@proton.me>
2026-03-11 17:31:29 -0300

    Add files via upload

---
+++ 2026_03_11_ex_11/app.js
@@ -0,0 +1,4 @@
+let nome = prompt ('Digite seu nome: ')
+let idade = parseInt(prompt('Digite sua idade: '))
+document.getElementById('resultado').innerHTML = `Seu nome é ${nome}.`
+document.getElementById('resultado2').innerHTML = `Sua idade é ${idade} anos.`
