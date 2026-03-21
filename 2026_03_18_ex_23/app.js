630c528cdd5166d2ab0bc9fc1c74009d878b7620
madeosdev <matheosdev@proton.me>
2026-03-20 21:08:25 -0300

    Delete 2026_03_18_ex_23 directory

--- 2026_03_18_ex_23/app.js
+++
@@ -1,5 +0,0 @@
-let mesada = parseFloat(prompt('Qual é o valor da mesada?'))
-let economia = parseFloat(prompt('Qual é o valor que você deseja economizar?'))
-let restante = mesada - economia 
-let formatado = new Intl.NumberFormat('pt-BR', {style:'currency', currency:'BRL'}).format(restante)
-document.getElementById('resultado').innerHTML = `O valor restante é ${formatado}.`
