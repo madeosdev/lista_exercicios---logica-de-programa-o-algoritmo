1d062b6a59f893e19dcf2a072253015264de3e6d
Matheos Moraes <matheosdev@proton.me>
2026-04-01 14:59:03 -0300

    Delete 01_04_2026_ex_42 directory

--- 01_04_2026_ex_42/app.js
+++
@@ -1,4 +0,0 @@
-let resultado = document.getElementById('resultado')
-let idade_anos = parseFloat (prompt('Qual a sua idade (em anos)?' ))
-let idade_dias = idade_anos*365
-resultado.innerHTML = `Você tem ${idade_anos} anos, que convertendo em dias, serão ${idade_dias} dias.`
