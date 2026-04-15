829296308ba75d379f09159654fd1f300bed5853
Matheos Moraes <matheosdev@proton.me>
2026-04-15 10:46:59 -0300

    Delete 2026_08_04_ex_61 directory

--- 2026_08_04_ex_61/app.js
+++
@@ -1,9 +0,0 @@
-let resultado = document.getElementById('resultado')
-let num = parseFloat(prompt('Digite um número: '))
-if (num < 0) {
-    resultado.innerHTML = 'Número inválido. Insira apenas números positivos.'
-} else if (num%2 === 0) {
-    resultado.innerHTML = 'O número é par.'
-} else if (num%2 !== 0) {
-    resultado.innerHTML = 'O número é ímpar.'
-}
