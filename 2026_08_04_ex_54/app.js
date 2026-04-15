1e4721588134a17f787b40bec34622f1f09df4d7
Matheos Moraes <matheosdev@proton.me>
2026-04-15 10:44:35 -0300

    Delete 2026_08_04_ex_54 directory

--- 2026_08_04_ex_54/app.js
+++
@@ -1,7 +0,0 @@
-let resultado = document.getElementById('resultado')
-let num = parseInt(prompt("Digite um número: "))
-if (num%2 == 0) {
-    resultado.innerHTML = `O número ${num} é par.`
-} else {
-    resultado.innerHTML = `O número ${num} é ímpar.`
-}
