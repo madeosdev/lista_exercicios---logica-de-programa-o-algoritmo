7e972a60c798f0bcf4db938d05bb8091b654442e
Matheos Moraes <matheosdev@proton.me>
2026-05-13 17:27:42 -0300

    Add files via upload

---
+++ 2026_05_13_ex_18/app.js
@@ -0,0 +1,5 @@
+let div = document.getElementById('resultado')
+let palavras = ['uranio', 'mercurio', 'xenonio', 'hidrogenio', 'helio', 'plutonio', 'tungstenio']
+
+let mediana = palavras[Math.floor(palavras.length/2)]
+console.log(mediana)
