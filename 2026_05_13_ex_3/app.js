2da4d5d79a1099edb08a87eee259fa3db349e648
Matheos Moraes <matheosdev@proton.me>
2026-05-19 15:31:42 -0300

    Delete 2026_05_13_ex_3 directory

--- 2026_05_13_ex_3/app.js
+++
@@ -1,4 +0,0 @@
-let div = document.getElementById('resultado')
-let palavra = 'Pneumoultramicroscopicosilicovulcanoconiose'
-let palavra_metade = palavra.substring(0, (palavra.length/2))
-div.innerHTML = `Palavra: ${palavra}<br>Metade da palavra: ${palavra_metade}`
